516fac19577cdd2665d32677089f2b8281007cef
Bukoplot <e.b.jarochowska@uu.nl>
2023-03-15 21:56:57 +0100

    Correct the license

--- app.R
+++ app.R
@@ -1,3 +1,11 @@
+# Copyright 2023 Niklas Hohmann and Emilia Jarochowska
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+
 library(shiny)
 source("src/condensator_plot.R")
 source("src/condensator.R")
