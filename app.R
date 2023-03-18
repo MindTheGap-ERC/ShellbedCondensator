352582e66c7a88324198899322412dbb960f9c69
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-03-18 15:30:56 +0100

    + add ref to Apache license

--- app.R
+++ app.R
@@ -130,8 +130,8 @@ ui <- navbarPage(
           tags$li(HTML("Hohmann, N. (2020). R package \"DAIME\". <i>Comprehensive R Archive Network (CRAN)</i>. URL <a href=https://cran.r-project.org/web/packages/DAIME/index.html target=\"_blank\" > cran.r-project.org/web/packages/DAIME </a>, <a href=http://doi.org/10.5281/zenodo.3702552 target=\"_blank\" > doi.org/10.5281/zenodo.3702552 </a>"))
         ),
         tags$h4("Code Availability"),
-        tags$p(HTML("The code for this app is available under <a href=https://github.com/MindTheGap-ERC/ShellbedCondensator target=\"_blank\" > github.com/MindTheGap-ERC/ShellbedCondensator </a> ")),
-        tags$h4("License"),
+        tags$p(HTML('The code for this app is available under <a href="https://github.com/MindTheGap-ERC/ShellbedCondensator"> github.com/MindTheGap-ERC/ShellbedCondensator </a>')),
+        tags$h4("License "),
         tags$p(HTML('<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>. The code used is licensed under a <a rel="license" href="http://www.apache.org/licenses/LICENSE-2.0"> Apache 2.0 License</a>.')),
         tags$h4("Funding"),
         tags$p("Online access to the Shellbed Condensator is made possible by the IDUB programme of the University of Warsaw (Grant BOB-IDUB-622-18/2022).
